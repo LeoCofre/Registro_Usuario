6034410cf10343f078e69027efa28be63d7952f5
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-06-20 22:12:12 -0400

    Vista y funcionalidad a los componentes, por ahora no muestra el imageView.

--- app/src/main/java/cl/awakelab/registrousuario/MainActivity.java
+++ app/src/main/java/cl/awakelab/registrousuario/MainActivity.java
@@ -3,6 +3,10 @@ package cl.awakelab.registrousuario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText textName;
+        EditText textLastName;
+        EditText textEmail;
+        EditText textPassword;
+        Button buttonCreateAccount;
+
+        textName = findViewById(R.id.editTextName);
+        textLastName = findViewById(R.id.editTextLastName);
+        textEmail = findViewById(R.id.editTextEmail);
+        textPassword = findViewById(R.id.editTextPassword);
+        buttonCreateAccount = findViewById(R.id.buttonCreateAccount);
+
+        buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = textName.getText().toString();
+                String lastName = textLastName.getText().toString();
+                String email = textEmail.getText().toString();
+                String password = textPassword.getText().toString();
+
+                if(name.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || !email.contains("@") ) {
+                    Toast.makeText(getBaseContext(), "Faltan campos por completar", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String textoToast = "Bienvenido " + name + " " + lastName + " " + "Email" + email + " " + "contraseña" + password;
+                Toast.makeText(getBaseContext(), textoToast, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
