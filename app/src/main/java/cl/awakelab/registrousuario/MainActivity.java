e3e4cb1fa1075b2bfaa29473d02e5eae8ae30c96
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-06-27 22:19:18 -0400

    Ejercio 10, inflamos la vista a través de View binding

--- app/src/main/java/cl/awakelab/registrousuario/MainActivity.java
+++ app/src/main/java/cl/awakelab/registrousuario/MainActivity.java
@@ -1,46 +1,39 @@
 package cl.awakelab.registrousuario;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import cl.awakelab.registrousuario.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        EditText textName;
-        EditText textLastName;
-        EditText textEmail;
-        EditText textPassword;
-        Button buttonCreateAccount;
 
-        textName = findViewById(R.id.editTextName);
-        textLastName = findViewById(R.id.editTextLastName);
-        textEmail = findViewById(R.id.editTextEmail);
-        textPassword = findViewById(R.id.editTextPassword);
-        buttonCreateAccount = findViewById(R.id.buttonCreateAccount);
-
-        buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
+        binding.buttonCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = textName.getText().toString();
-                String lastName = textLastName.getText().toString();
-                String email = textEmail.getText().toString();
-                String password = textPassword.getText().toString();
+                String name = binding.editTextName.getText().toString();
+                String lastName = binding.editTextLastName.getText().toString();
+                String email = binding.editTextEmail.getText().toString();
+                String password = binding.editTextPassword.getText().toString();
 
-                if(name.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || !email.contains("@") ) {
+                if (name.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || !email.contains("@")) {
                     Toast.makeText(getBaseContext(), "Faltan campos por completar", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                String textoToast = "Bienvenido " + name + " " + lastName + " " + "Email" + email + " " + "contraseña ********" ;
+                String textoToast = "Bienvenido " + name + " " + lastName + " " + "Email" + email + " " + "contraseña ********";
                 Toast.makeText(getBaseContext(), textoToast, Toast.LENGTH_LONG).show();
             }
         });
